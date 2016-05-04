6e5a4bce439d5d30caaed017e001a38efb9a73c9
Lloyd Clave <lloyd.clave@gmail.com>
2016-05-04 10:04:08 -0400

    4.4 Release 1; added gathering data code for vampire interview

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you? What year were you born?"
+ageAndBirth = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlicBread = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance?"
+healthInsurance = gets.chomp
