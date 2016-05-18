0258725c61451d04a811812c7d855e4830503e6b
Lloyd Clave <lloyd.clave@gmail.com>
2016-05-18 16:43:12 -0400

    completed release 3 for 6.5 - converted standalone module to a mixin, created two classes to include mixin, added driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,16 +1,54 @@
+# 6.5 Release 1
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#   	words.upcase + "!!!!!" + " :D"
+#   end
+# end
+
+# DRIVER CODE
+# puts "yell_angrily test:"
+# puts Shout.yell_angrily("Nooo")
+# puts "------------------------------"
+# puts "yelling_happily test:"
+# puts Shout.yelling_happily("yay")
+
+
+#6.5 Release 3 - Mixin
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yell_angrily(words)
+    words.upcase + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
+  def yelling_happily(words)
   	words.upcase + "!!!!!" + " :D"
   end
 end
 
+class Lion
+	include Shout
+end
+
+class Announcer
+	include Shout
+end
+
 # DRIVER CODE
-puts "yell_angrily test:"
-puts Shout.yell_angrily("Grrr")
-puts "------------------------------"
-puts "yelling_happily test:"
-puts Shout.yelling_happily("yay")
+
+lion = Lion.new
+print "The lion yelled angrily: "
+puts lion.yell_angrily("Grrrrrrrrrrr")
+print "The lion yelled happily: "
+puts lion.yelling_happily("roooooaaaarrrrr")
+
+puts "\n"
+
+announcer = Announcer.new
+print "The announcer yelled happily: "
+puts announcer.yelling_happily("ladies and gentlemen")
+print "The announcer yelled angrily: "
+puts announcer.yell_angrily("disappointing")
+
